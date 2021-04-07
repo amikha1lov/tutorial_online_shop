0567f3a65998f2ac5c5553c00f9abe52351ad63e
amikha1lov <mikha1lov@rambler.ru>
2021-04-08 02:29:34 +0300

    created components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    hello
+    <v-main-wrapper />
   </div>
 </template>
 
 <script>
-
+import vMainWrapper from './components/v-main-wrapper'
 
 export default {
   name: 'App',
   components: {
-  
+    vMainWrapper
   }
 }
 </script>
