17c391411794ca658b607378ffa5920530b344ef
amikha1lov <mikha1lov@rambler.ru>
2021-04-08 12:22:17 +0300

    added Vuex and Axios

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
         <h1>Catalog</h1>
         <div class="v-catalog__list">
             <v-catalog-item 
-                v-for="product in products"
+                v-for="product in PRODUCTS"
                 :key="product.article"
                 :product_data="product"
                 @sendDataToParent="showChildArticleInConsole"
@@ -15,7 +15,7 @@
 
 <script>
 import vCatalogItem from './v-catalog-item';
-
+import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: 'v-catalog',
@@ -23,63 +23,24 @@ export default {
   props: {},
   data() {
       return {
-          products: [
-                {
-                image: "1.jpg",
-                name: "T-shirt 1",
-                price: 100,
-                article: "T1",
-                available: true,
-                category: "Мужские"
-                },
-                {
-                image: "2.jpg",
-                name: "T-shirt 2",
-                price: 150,
-                article: "T2",
-                available: true,
-                category: "Женские"
-                },
-                {
-                image: "3.jpg",
-                name: "T-shirt 3",
-                price: 200,
-                article: "T3",
-                available: false,
-                category: "Женские"
-                },
-                {
-                image: "4.jpg",
-                name: "T-shirt 4",
-                price: 300,
-                article: "T4",
-                available: true,
-                category: "Мужские"
-                },
-                {
-                image: "5.jpg",
-                name: "T-shirt 5",
-                price: 400,
-                article: "T5",
-                available: false,
-                category: "Женские"
-                },
-                {
-                image: "6.jpeg",
-                name: "T-shirt 6",
-                price: 500,
-                article: "T6",
-                available: true,
-                category: "Женские"
-                }
-            ]
+          
       }
   },
-  computed:{},
+  computed:{
+      ...mapGetters([
+          'PRODUCTS'
+      ]),
+  },
   methods:{
+      ...mapActions([
+          'GET_PRODUCTS_FROM_API'
+      ]),
       showChildArticleInConsole(data) {
           console.log(data);
       }
+  },
+  mounted() {
+      this.GET_PRODUCTS_FROM_API()
   } 
 
 }
