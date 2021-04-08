a30a7587e747baf1b500247f16a9323f560381c1
amikha1lov <mikha1lov@rambler.ru>
2021-04-08 03:44:30 +0300

    Passing data between components

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style land="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="v-catalog">
         <h1>Catalog</h1>
-        <v-catalog-item></v-catalog-item>
+        <div class="v-catalog__list">
+            <v-catalog-item 
+                v-for="product in products"
+                :key="product.article"
+                :product_data="product"
+                @sendDataToParent="showChildArticleInConsole"
+            
+            />
+        </div>
     </div>
 </template>
 
@@ -14,12 +22,77 @@ export default {
   components:{vCatalogItem},
   props: {},
   data() {
-      return {}
+      return {
+          products: [
+                {
+                image: "1.jpg",
+                name: "T-shirt 1",
+                price: 100,
+                article: "T1",
+                available: true,
+                category: "Мужские"
+                },
+                {
+                image: "2.jpg",
+                name: "T-shirt 2",
+                price: 150,
+                article: "T2",
+                available: true,
+                category: "Женские"
+                },
+                {
+                image: "3.jpg",
+                name: "T-shirt 3",
+                price: 200,
+                article: "T3",
+                available: false,
+                category: "Женские"
+                },
+                {
+                image: "4.jpg",
+                name: "T-shirt 4",
+                price: 300,
+                article: "T4",
+                available: true,
+                category: "Мужские"
+                },
+                {
+                image: "5.jpg",
+                name: "T-shirt 5",
+                price: 400,
+                article: "T5",
+                available: false,
+                category: "Женские"
+                },
+                {
+                image: "6.jpeg",
+                name: "T-shirt 6",
+                price: 500,
+                article: "T6",
+                available: true,
+                category: "Женские"
+                }
+            ]
+      }
   },
-  computed:{} 
+  computed:{},
+  methods:{
+      showChildArticleInConsole(data) {
+          console.log(data);
+      }
+  } 
+
 }
 </script>
 
-<style>
+<style lang="scss">
+.v-catalog {
+    &__list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+}
 
 </style>
