ffc6be09e9e0c7d9f9095a79ddab43a1d47eee6e
amikha1lov <mikha1lov@rambler.ru>
2021-04-08 16:24:13 +0300

    added Vue-Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './vuex/store';
+import router from './router/router';
 
 import './assets/styles/styles.scss'
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  store
+  store,
+  router
 }).$mount('#app')
